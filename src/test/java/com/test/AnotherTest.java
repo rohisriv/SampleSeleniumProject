9b2a02c95b7439f74b4ae2d2b7883f0209013bb9
Rohini.Srivastava <Rohini.Srivastava@OTM023829285.mshome.net>
2022-07-04 14:11:39 -0500

    committing to branch X

---
+++ src/test/java/com/test/AnotherTest.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class AnotherTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//This is class is only for branch X and not the master branch
+
+	}
+
+}
