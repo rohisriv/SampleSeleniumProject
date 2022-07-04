8660b649f1d537cb27d9771a4fc14adf88f9c156
Rohini.Srivastava <Rohini.Srivastava@OTM023829285.mshome.net>
2022-07-04 14:22:58 -0500

    Committing the changes made to today test.java

--- src/test/java/com/test/TodayTest.java
+++ src/test/java/com/test/TodayTest.java
@@ -1,1 +1,13 @@
-This is a new java file
+package com.test;
+
+public class TodayTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//This is class is only for branch X and not the master branch
+
+	}
+
+}
+
