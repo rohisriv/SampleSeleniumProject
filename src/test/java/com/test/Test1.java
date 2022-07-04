201a0b73628b3c2b8cff6dede15256bb9d671c73
Rohini.Srivastava <Rohini.Srivastava@OTM023829285.mshome.net>
2022-07-04 14:31:50 -0500

    Committing Test1 and Test2 changes

--- src/test/java/com/test/Test1.java
+++ src/test/java/com/test/Test1.java
@@ -1,1 +1,13 @@
+package com.test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//This class has been added by ROHINI and will work just fine
+
+	}
+
+}
 
--- src/test/java/com/test/Test2.java
+++ src/test/java/com/test/Test2.java
@@ -1,1 +1,12 @@
+package com.test;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//This class has been added by ARJUn and works just fine
+	}
+
+}
 
